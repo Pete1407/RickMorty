d2776d90e0a3e5da5119fe494df26fbe9b974815
Pittawat <pete.pittawas@gmail.com>
2021-07-26 22:06:40 +0700

    update

---
+++ app/src/main/java/com/example/rickmorty/base/BaseActivity.kt
@@ -0,0 +1,18 @@
+package com.example.rickmorty.base
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity(){
+    private  var viewBinding : View? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun setContentView(view: View?) {
+        super.setContentView(view)
+        viewBinding = view
+    }
+}
---
+++ app/src/main/java/com/example/rickmorty/base/BaseFragment.kt
@@ -0,0 +1,6 @@
+package com.example.rickmorty.base
+
+import androidx.fragment.app.Fragment
+
+open class BaseFragment : Fragment(){
+}
