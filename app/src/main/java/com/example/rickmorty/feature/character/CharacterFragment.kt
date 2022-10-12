f0cea6764f522db1e58a95e881bb4c9dfdaec679
Pittawat <pete.pittawas@gmail.com>
2022-10-12 14:18:56 +0700

    test

--- app/src/main/java/com/example/rickmorty/feature/character/CharacterFragment.kt
+++ app/src/main/java/com/example/rickmorty/feature/character/CharacterFragment.kt
@@ -218,6 +218,7 @@ class CharacterFragment : BaseFragment(),CustomState{
         return dataShuffled.subList(0,10).toMutableList()
     }
 
+    // refresh
     private fun refreshList(){
         binding.refreshLayout.isRefreshing = false
         numberPage = ""
